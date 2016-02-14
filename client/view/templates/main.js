9342f15ff1ba22a452f04e481a2f38922c3a712c
Nazarii <nazarii.strohush@gmail.com>
2016-02-14 16:01:10 +0200

    Додавання карти Mapbox

--- client/view/templates/main.js
+++ client/view/templates/main.js
@@ -1,3 +1,4 @@
+map = null;
 Template.main.onRendered(function () {
     Mapbox.debug = true;
     Mapbox.load({
@@ -5,12 +6,33 @@ Template.main.onRendered(function () {
     });
 
     this.autorun(function () {
+      
       if (Mapbox.loaded()) {
         mapboxgl.accessToken = 'pk.eyJ1IjoibmF6YXJpaXN0cm9odXNoIiwiYSI6ImNpa2lyMXp6czAwNDF3am00ejhvcnZzcWQifQ.D1neZSb0jjMlVM0GArJ80w';
-        var map = new mapboxgl.Map({
+        map = new mapboxgl.Map({
           container: 'map',
           style: 'mapbox://styles/nazariistrohush/cikiu7r5c005kcilu1v5egwhw',
         });
+        //map.setCenter("35.689488","35.689488");
       }
     });
+  });
+
+Template.main.events({
+  'click .s4': function(e) {
+    e.preventDefault();
+    params = {
+      ll: "35.68949, 139.69171", //Your location use the geo result here
+      query: "school",
+      limit: 10, //the limit of the query
+    }
+
+    Foursquare.find(params, function(error, result) {
+      if (error) {
+        console.log(error);
+      }
+      console.log(result);
+    });
+
+    }
   });
